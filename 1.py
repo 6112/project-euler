1133d9ea273728389aac02ddeee388cac085a8cb
minimus <ashlebede@gmail.com>
2013-12-19 17:25:21 -0500

    using MAX_VALUE instead of a constant in problem 1

--- 1.py
+++ 1.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+## SOLVED 19/12/13
+## 233168
 
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
 # get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+MAX_VALUE = 1000
+
 def euler ():
-    return sum ([n for n in range (0, 1000) if n % 5 == 0 or n % 3 == 0])
+    return sum ([n for n in range (0, MAX_VALUE) if n % 5 == 0 or n % 3 == 0])
 
 print (euler ())
