f76dc1de3738e311fb7912b32d0382bdcef70c3d
minimus <ashlebede@gmail.com>
2013-12-19 20:48:33 -0500

    problem 10 solved

---
+++ 10.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+## SOLVED 19/12/13
+## 142913828922
+
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+
+from modules.prime import *
+
+HIGHEST_VALUE = 2000000
+
+def euler ():
+    return sum (primes (HIGHEST_VALUE))
+
+print (euler ())
