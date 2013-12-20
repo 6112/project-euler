72ffdfb3e72381e75d5643b71d5c01b3ff4f4f9d
minimus <ashlebede@gmail.com>
2013-12-19 20:23:45 -0500

    added generate_n_primes() and n_primes()

--- modules/prime.py
+++ modules/prime.py
@@ -7,15 +7,13 @@ prime_list = [2, 3]
 def generate_primes (highest_value):
     """Generates the list of primes prime_list which contains all prime numbers
     lower than a given number."""
-    number = prime_list [-1]
+    number = prime_list [-1] + 2
     highest_test_prime = ceil (sqrt (highest_value))
     test_primes = []
     i = 0
     while i < len (prime_list) and prime_list [i] <= highest_test_prime:
         test_primes.append (prime_list [i])
         i += 1
-    def divides_by (prime):
-        return number % prime == 0
     while number <= highest_value:
         if all (number % prime != 0 for prime in test_primes):
             prime_list.append (number)
@@ -24,13 +22,37 @@ def generate_primes (highest_value):
         number += 2
     return prime_list
 
+def generate_n_primes (number_of_primes):
+    """Generates the list of primes prime_list which contains all the first n
+    prime numbers."""
+    number = prime_list [-1] + 2
+    prime_count = len (prime_list)
+    while prime_count < number_of_primes:
+        is_a_prime = True
+        highest_test_prime = ceil (sqrt (number))
+        i = 0
+        while i < len (prime_list) and prime_list [i] <= highest_test_prime:
+            is_a_prime = is_a_prime and number % prime_list [i] != 0
+            i += 1
+        if is_a_prime:
+            prime_list.append (number)
+            prime_count += 1
+        number += 2
+    return prime_list
+
 def primes (highest_value):
-    """Returns an iterator for all primes up to a maximum value."""
+    """Used as an iterator for all primes up to a maximum value."""
     generate_primes (highest_value)
     i = 0
     while i < len (prime_list) and prime_list [i] <= highest_value:
         yield prime_list [i]
         i += 1
+
+def n_primes (number_of_primes):
+    """Used as an iterator for the first n primes."""
+    generate_n_primes (number_of_primes)
+    for i in range (number_of_primes):
+        yield prime_list [i]
 
 def is_prime (number):
     """Returns True if a given integer is a prime number."""
