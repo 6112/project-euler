2a959e349cfe6535c2777ac89df581b0dc17938c
minimus <ashlebede@gmail.com>
2013-12-19 20:05:49 -0500

    fixed problem 5

--- 5.py
+++ 5.py
@@ -14,7 +14,7 @@ HIGHEST_DIVISOR = 20
 
 def euler ():
     zipped_factors = {}
-    for number in range (1, HIGHEST_DIVISOR):
+    for number in range (1, HIGHEST_DIVISOR + 1):
         factors = dictionary_prime_factors (number)
         for factor, occurences in factors.items ():
             if (not factor in zipped_factors
