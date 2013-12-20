9b57f0b5d7c6117572171e2b142553ffc138fe66
minimus <ashlebede@gmail.com>
2013-12-19 20:02:37 -0500

    problem 5 solved

---
+++ 5.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+## SOLVED 19/12/13
+## 232792560
+
+# 2520 is the smallest number that can be divided by each of the numbers from 1
+# to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+
+from modules.prime import *
+
+HIGHEST_DIVISOR = 20
+
+def euler ():
+    zipped_factors = {}
+    for number in range (1, HIGHEST_DIVISOR):
+        factors = dictionary_prime_factors (number)
+        for factor, occurences in factors.items ():
+            if (not factor in zipped_factors
+              or zipped_factors [factor] < occurences):
+                zipped_factors [factor] = occurences
+    product = 1
+    for factor, power in zipped_factors.items ():
+        product *= factor ** power
+    return product
+
+print (euler ())
