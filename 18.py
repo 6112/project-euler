f6c08a06f8d1a0ed1119f3e5b001537b0cd6e28c
minimus <ashlebede@gmail.com>
2013-12-21 11:00:04 -0500

    problem 18 solved

---
+++ 18.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+## SOLVED 21/12/13
+## 1074
+
+# By starting at the top of the triangle below and moving to adjacent numbers on
+# the row below, the maximum total from top to bottom is 23.
+
+#    3
+#   7 4
+#  2 4 6
+# 8 5 9 3
+
+# That is, 3 + 7 + 4 + 9 = 23.
+
+# Find the maximum total from top to bottom of the triangle below:
+
+from modules.file import *
+
+def euler ():
+    pyramid = list_from_file ('data/18.txt')
+    for y in range (len (pyramid) - 2, -1, -1):
+        for x in range (y + 1):
+            pyramid [y][x] += max (pyramid [y + 1][x], pyramid [y + 1][x + 1])
+    return pyramid [0][0]
+
+print (euler ())
