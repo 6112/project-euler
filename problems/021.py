a3257d5d7dd5b76e4f2a9e8881e4ec91c913d4f1
minimus <ashlebede@gmail.com>
2013-12-21 14:41:25 -0500

    problem 21 solved

---
+++ problems/021.py
@@ -0,0 +1,29 @@
+## SOLVED 21/12/13
+## 31626
+
+# Let d(n) be defined as the sum of proper divisors of n (numbers less than n
+# which divide evenly into n).
+
+# If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and
+# each of a and b are called amicable numbers.
+
+# For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55
+# and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71
+# and 142; so d(284) = 220.
+
+# Evaluate the sum of all the amicable numbers under 10000.
+
+from modules.prime import *
+
+MAX_VALUE = 10000
+
+def euler ():
+    accumulator = 0
+    for a in range (1, 10000):
+        b = d (a)
+        if b != 0 and d (b) == a and a < b:
+            accumulator += b + a
+    return accumulator
+
+def d (number):
+    return sum (divisors (number)) - number
