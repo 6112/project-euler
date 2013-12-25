7a2f66a1ff8a03f253535ff8dabb421f0335835f
minimus <ashlebede@gmail.com>
2013-12-25 13:02:55 -0500

    added rotate(), rotations() to modules.sequence

--- modules/sequence.py
+++ modules/sequence.py
@@ -7,3 +7,17 @@ def permutations (tokens):
         rest = tokens [: index] + tokens [index + 1:]
         for permutation in permutations (rest):
             yield [first] + permutation
+
+def rotate (tokens):
+    """Returns a rotated sequence from the given sequence. All elements are
+    moved one position to the right, and the last element is placed at the
+    beginning."""
+    return tokens [-1 :] + tokens [: -1]
+
+def rotations (tokens):
+    """Used as an iterator for all rotations of a sequence, as per the rotate()
+    function."""
+    rotation = tokens
+    for iterator in range (len (tokens)):
+        yield rotation
+        rotation = rotate (tokens)
