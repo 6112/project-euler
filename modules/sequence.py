d9040c70c879d67aee4ef549049d07b02be76a8e
minimus <ashlebede@gmail.com>
2013-12-25 14:21:59 -0500

    added left_truncations() and right_truncations()

--- modules/sequence.py
+++ modules/sequence.py
@@ -8,6 +8,20 @@ def permutations (tokens):
         for permutation in permutations (rest):
             yield [first] + permutation
 
+def left_truncations (tokens):
+    """Used as an iterator for all truncations of a sequence, from the left.
+    For instance, left_truncations('123') yields '123', '12', and '1'."""
+    while tokens:
+        yield tokens
+        tokens = tokens [: -1]
+
+def right_truncations (tokens):
+    """Used as an iterator for all truncations of a sequence, from the right.
+    For instance, right_truncations('123' yields '123', '23', and '3'."""
+    while tokens:
+        yield tokens
+        tokens = tokens [1 :]
+
 def rotate (tokens):
     """Returns a rotated sequence from the given sequence. All elements are
     moved one position to the right, and the last element is placed at the
