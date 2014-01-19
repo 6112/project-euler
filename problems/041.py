3ee5c3ec065c2187a13a3727c073c8e9ebc289d5
minimus <ashlebede@gmail.com>
2014-01-19 00:57:56 -0500

    problem 41 solved

---
+++ problems/041.py
@@ -0,0 +1,29 @@
+# encoding=utf-8
+## SOLVED 19/01/14
+## 7652413
+
+# We shall say that an n-digit number is pandigital if it makes use of all the
+# digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is
+# also prime.
+
+# What is the largest n-digit pandigital prime that exists?
+
+import modules.prime as prime
+import modules.sequence as sequence
+
+def euler ():
+    # highest pandigital prime found so far
+    highest_pandigital = 0
+    # for number of digits from 4 to 9
+    for length in range (4, 9):
+        # generate the list of digits from 1 to n
+        digits = list (range (1, length + 1))
+        # for each permutation of these digits
+        for permutation in sequence.permutations (digits):
+            number = int (''.join (str (digit) for digit in permutation))
+            # check if the number is prime
+            if prime.is_prime (number):
+                # set the new value of the highest pandigital prime if needed
+                highest_pandigital = max (number, highest_pandigital)
+    # return the highest pandigital prime found
+    return highest_pandigital
