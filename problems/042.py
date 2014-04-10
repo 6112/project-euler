9ef446c904eb7c50c62804acac753eab9407c572
minimus <ashlebede@gmail.com>
2014-04-10 13:43:24 -0400

    problem 42 solved

---
+++ problems/042.py
@@ -0,0 +1,39 @@
+#encoding=utf-8
+## SOLVED 10/04/14
+## 162
+
+# The nth term of the sequence of triangle numbers is given by, tn = ½n(n+1); so
+# the first ten triangle numbers are:
+ 
+# 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+
+# By converting each letter in a word to a number corresponding to its
+# alphabetical position and adding these values we form a word value. For
+# example, the word value for SKY is 19 + 11 + 25 = 55 = t10. If the word
+# value is a triangle number then we shall call the word a triangle word.
+
+# Using words.txt (right click and 'Save Link/Target As...'), a 16K text file
+# containing nearly two-thousand common English words, how many are triangle
+# words?
+
+import modules.file as fileutils
+
+def euler():
+    triangles = set()
+    n = 1
+    highest_triangle = 0
+    while highest_triangle < 1000:
+        highest_triangle = n * (n + 1) // 2
+        triangles.add(highest_triangle)
+        n += 1
+    words = fileutils.flattened_list_from_file('data/042.txt',
+            separator = ',', convert_to = str)
+    words = [word.replace('"', '') for word in words]
+    triangle_word_count = 0
+    for word in words:
+        if word_to_int(word) in triangles:
+            triangle_word_count += 1
+    return triangle_word_count
+
+def word_to_int(word):
+    return sum(ord(letter) - ord('a') + 1 for letter in word.lower())
