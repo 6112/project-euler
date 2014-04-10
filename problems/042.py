714f9125a2ac8b230248df304ec55d864b8cf8be
minimus <ashlebede@gmail.com>
2014-04-10 14:26:11 -0400

    commented and documented 42 & 43

--- problems/042.py
+++ problems/042.py
@@ -18,22 +18,35 @@
 
 import modules.file as fileutils
 
+# arbitrary value for the highest reachable triangle number
+HIGHEST_TRIANGLE = 1000
+
 def euler():
+    # set of the triangle numbers until an arbitrary maximum number
     triangles = set()
+    # generate triangle numbers
     n = 1
     highest_triangle = 0
-    while highest_triangle < 1000:
+    while highest_triangle < HIGHEST_TRIANGLE:
         highest_triangle = n * (n + 1) // 2
         triangles.add(highest_triangle)
         n += 1
+    # read the words and put them into a list of strings
     words = fileutils.flattened_list_from_file('data/042.txt',
             separator = ',', convert_to = str)
+    # strip the quote-sign from the strings, leaving only the word
     words = [word.replace('"', '') for word in words]
+    # accumulator for the final answer, the number of triangle words
     triangle_word_count = 0
+    # count the number of triangle words
     for word in words:
         if word_to_int(word) in triangles:
             triangle_word_count += 1
+    # return it
     return triangle_word_count
 
 def word_to_int(word):
+    """Returns the sum of the 'letter value' of each letter in the word.
+
+    ('a' = 1, 'b' = 2, 'c' = 3, ...)"""
     return sum(ord(letter) - ord('a') + 1 for letter in word.lower())
--- problems/043.py
+++ problems/043.py
@@ -19,23 +19,36 @@
 
 # Find the sum of all 0 to 9 pandigital numbers with this property.
 
-import modules.sequence as sequence
-
 def euler():
+    # accumulator for the sum of these special pandigitals, final answer
     accumulator = 0
-    for pandigital in generate_pandigitals():
+    for pandigital in generate_valid_pandigitals():
         accumulator += list_to_int(pandigital)
     return accumulator
 
 def list_to_int(digits):
+    """Converts a list of integers corresponding to digits to a number.
+
+    e.g.:
+        list_to_int([4, 9, 2]) == 492.
+
+    Args:
+        digits: list of integers, which are the digits of the number."""
+    # start at 0
     accumulator = 0
     for digit in digits:
+        # move the previous digits to the left
         accumulator *= 10
+        # append this digit
         accumulator += int(digit)
+    # return the number
     return accumulator
 
-def generate_pandigitals():
+def generate_valid_pandigitals():
+    """Yields all the pandigitals that fit the criterion in the question."""
+    # set of available digits (each from 0 to 9)
     digits = set(range(0, 10))
+    # for each possibility for first, second, and third digit
     for first_digit in digits:
         first_available_digits = set(digits)
         first_available_digits.remove(first_digit)
@@ -45,18 +58,42 @@ def generate_pandigitals():
             for third_digit in second_available_digits:
                 third_available_digits = set(second_available_digits)
                 third_available_digits.remove(third_digit)
-                for pandigital in generate_pandigitals_helper([second_digit, third_digit],
-                        [2, 3, 5, 7, 11, 13, 17],
+                # for each pandigital with those starting digits
+                for pandigital in pandigitals_helper(
+                        [second_digit, third_digit], # last 2 digits
+                        [2, 3, 5, 7, 11, 13, 17], # divisions to use, in order
                         third_available_digits):
+                    # yield the whole pandigital 'list' to the main function
                     yield [first_digit, second_digit, third_digit] + pandigital
 
-def generate_pandigitals_helper(pair, divisors, available):
+def pandigitals_helper (pair, divisors, available):
+    """Used as a helper for generate_valid_pandigitals(), do not use directly.
+
+    Selects the next possible digit that validates the next division and
+    'yields' it recursively.
+
+    Args:
+        pair: 2-element list containing the previous two digits.
+        divisors: list containing the divisors that should be satisfied for
+        the rest of the digits, in order.
+        available: set containing the available digits for generating a number
+        that is still pandigital."""
+    # the current divisor to use
     divisor = divisors[0]
+    # for each possible digit combination
     for last_digit in available:
+        # if that combination is a multiple of the divisor, recurse
         if (pair[0] * 100 + pair[1] * 10 + last_digit) % divisor == 0:
+            # if this is the last digit
             if len(divisors) == 1:
+                # yield a list of the last digit only
                 yield [last_digit]
             else:
-                for pandigital in generate_pandigitals_helper([pair[1], last_digit],
-                        divisors[1:], available - {last_digit}):
+                # for each possible combination for the rest of the digits
+                for pandigital in pandigitals_helper(
+                        [pair[1], last_digit], # last 2 digits
+                        divisors[1:], # divisions to use, in order
+                        available - {last_digit} # digits still available
+                        ):
+                    # yield the last digit followed by the rest
                     yield [last_digit] + pandigital
