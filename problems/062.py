b812d5d705516d2d1c2ef7d2438c75808e4de24e
minimus <ashlebede@gmail.com>
2014-11-18 22:43:26 -0500

    added problem description for 61, 62, 63

--- problems/062.py
+++ problems/062.py
@@ -2,6 +2,13 @@
 ## SOLVED 18/11/14
 ## 127035954683
 
+# The cube, 41063625 (3453), can be permuted to produce two other cubes:
+# 56623104 (3843) and 66430125 (4053). In fact, 41063625 is the smallest cube
+# which has exactly three permutations of its digits which are also cube.
+
+# Find the smallest cube for which exactly five permutations of its digits are
+# cube.
+
 from modules.sequence import is_permutation
 from math import ceil
 
--- problems/063.py
+++ problems/063.py
@@ -2,6 +2,11 @@
 ## SOLVED 18/11/14
 ## 49
 
+# The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit
+# number, 134217728=89, is a ninth power.
+
+# How many n-digit positive integers exist which are also an nth power?
+
 from math import log
 
 def euler():
