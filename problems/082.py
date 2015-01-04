8e6bf402f845582148727aff398001a170c494f9
minimus <ashlebede@gmail.com>
2015-01-04 01:26:06 -0500

    problem 83 solved

--- problems/082.py
+++ problems/082.py
@@ -8,6 +8,8 @@
 # the left column and finishing in any cell in the right column, and only moving
 # up, down, and right, is indicated in red and bold; the sum is equal to 994.
 
+# [example...]
+
 # Find the minimal path sum, in matrix.txt (right click and "Save Link/Target
 # As..."), a 31K text file containing a 80 by 80 matrix, from the left column to
 # the right column.
